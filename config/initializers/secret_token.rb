b2e9700c8a0c48ed84b02717a3a0288e56806083
Bridget Greene <bridget.greene@student.ncirl.ie>
2012-06-26 21:42:57 +0100

    adding config files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smirkshirt::Application.config.secret_token = '73ac896a10d4ff2541d3beb2f08480db076e5a4fe49767bd78299317b4a9e094debb1ccc68505c722649f0575d08e282fa396ce24bc2b55d0cbb3fe73047a035'
