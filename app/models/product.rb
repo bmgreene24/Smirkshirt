cdd7eac064588e1b65e6e2cc313f0b52a01b5197
Bridget Greene <bridget.greene@student.ncirl.ie>
2012-06-26 21:28:56 +0100

    adding app files

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+  def self.search(search_query)
+    if search_query
+      find(:all,:conditions => ['name LIKE ?', "%#{ search_query}%"])
+    else
+      find(:all)
+    end
+  end
+end
